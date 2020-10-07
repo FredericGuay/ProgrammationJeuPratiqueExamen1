8a536385249f3501c99cabb0047f0827e2afebcf
Frederic_Guay <fredericguay@hotmail.ca>
2020-10-06 22:55:32 -0400

    Version initiale - Fin question 1

---
+++ Assets/Avion.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avion : MonoBehaviour
+{
+    private Rigidbody myRigidBody;
+    public int movementSpeed = 100;
+    [SerializeField] ParticleSystem Fumee = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Jump") != 0)
+        {
+
+            //myRigidBody.MovePosition(new Vector3(inputHorizontal, 0f, inputVertical) * Time.deltaTime);
+            myRigidBody.AddForce(new Vector3(Input.GetAxis("Jump") * 500, 0f, 0f) * Time.deltaTime);
+            //myRigidBody.AddRelativeForce(new Vector3(1, 0, 0) * Time.deltaTime);
+            //transform.Rotate(new Vector3(inputHorizontal, 0f, inputVertical) * Time.deltaTime);
+
+            //Vector3 movement = new Vector3(inputHorizontal, 0, 0) * Time.deltaTime * movementSpeed;
+            //myRigidBody.MovePosition(myRigidBody.position + movement);
+
+            //Fumee.Play();
+            Fumee.Emit(1);
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float inputHorizontal = Input.GetAxis("Horizontal");
+            myRigidBody.AddTorque(new Vector3((float)(-inputHorizontal * 0.1), 0f, 0f));
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            float inputVertical = Input.GetAxis("Vertical");
+            myRigidBody.AddTorque(new Vector3(0f, 0f, (float)(inputVertical * 0.1)));
+        }
+    }
+}
---
+++ Assets/Helice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helice : MonoBehaviour
+{
+    private Rigidbody myRigidBody;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Jump") != 0)
+        {
+            //transform.Rotate(new Vector3(Input.GetAxis("Jump"), 0f, 0f) * Time.deltaTime);
+            //Lui fonctionne, mais c'est pas comme ça qu'il est écrit de le faire
+            transform.Rotate(new Vector3(100f, 0f, 0f) * Time.deltaTime);
+            //myRigidBody.rot
+        }
+    }
+}
